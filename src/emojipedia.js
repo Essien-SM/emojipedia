f65066b2956043391d322e20f9eeaa993115938d
Essien-SM <stephenyessien@gmail.com>
2023-03-19 17:43:12 +0000

    emojipedia with mapping of component

---
+++ src/emojipedia.js
@@ -0,0 +1,67 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🤫",
+    name: "shushing face",
+    meaning:
+      "“Be quiet!” Or “This is a secret, don’t tell anyone.”"
+  },
+  {
+    id: 5,
+    emoji: "😪",
+    name: "sleepy face",
+    meaning:
+      "Tired or sleeping. Yes, that’s a snot bubble."
+  },
+  {
+    id: 6,
+    emoji: "🤧",
+    name: "sneezing face",
+    meaning:
+      "Sick, suffering from allergies, or crying into a tissue."
+  },
+  {
+    id: 7,
+    emoji: "🥵",
+    name: "hot face",
+    meaning:
+      "Sweating— from heat, spicy food, or because you saw something (or someone) you’re really into."
+  },
+  {
+    id: 8,
+    emoji: "🤠",
+    name: "cowboy hat face",
+    meaning:
+      "Ready for anything? This emoji signals that you’re down for an adventure. Giddy-up!"
+  },
+  {
+    id: 9,
+    emoji: "🧐",
+    name: "confused face",
+    meaning:
+      "Perfect for strange situations or texts you don’t quite know how to interpret."
+  },
+];
+
+export default emojipedia
